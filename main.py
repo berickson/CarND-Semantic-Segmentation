3c10c9db88c0c8a5d806591159055c4f581f2161
Brian Erickson <berickson@gmail.com>
2017-11-03 20:40:13 -0700

    First test passes

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+'''
+Udacity CarND semantic segmentation submission
+Brian Erickson
+'''
+
 import os.path
-import tensorflow as tf
-import helper
 import warnings
 from distutils.version import LooseVersion
+import tensorflow as tf
+import helper
 import project_tests as tests
 
 
 # Check TensorFlow Version
-assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
+assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), \
+    'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
 print('TensorFlow Version: {}'.format(tf.__version__))
 
 # Check for a GPU
@@ -18,22 +24,29 @@ else:
 
 
 def load_vgg(sess, vgg_path):
+    print("loading saved model from ", vgg_path)
     """
     Load Pretrained VGG Model into TensorFlow.
     :param sess: TensorFlow Session
     :param vgg_path: Path to vgg folder, containing "variables/" and "saved_model.pb"
-    :return: Tuple of Tensors from VGG model (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
+    :return: Tuple of Tensors from VGG model
+             (image_input, keep_prob, layer3_out, layer4_out, layer7_out)
     """
-    # TODO: Implement function
-    #   Use tf.saved_model.loader.load to load the model and weights
     vgg_tag = 'vgg16'
     vgg_input_tensor_name = 'image_input:0'
     vgg_keep_prob_tensor_name = 'keep_prob:0'
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    vgg = tf.get_default_graph()
+    input_tensor = vgg.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob_tensor = vgg.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out_tensor = vgg.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4_out_tensor = vgg.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7_out_tensor = vgg.get_tensor_by_name(vgg_layer7_out_tensor_name)
+
+    return input_tensor, keep_prob_tensor, layer3_out_tensor, layer4_out_tensor, layer7_out_tensor
 tests.test_load_vgg(load_vgg, tf)
 
 
